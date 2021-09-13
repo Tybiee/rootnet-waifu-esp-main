640741baaa2a4acf9638dc02af9750b33bdf4085
Tybie <tybiemeyer@gmail.com>
2021-09-13 17:31:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Waifu for rootNET
+WaifuESP puts cute anime girl overlays on players in your game.
+
+# Usage
+ - Head over to [actions](https://github.com/Tybie/rootnet-waifu-esp/actions) to download the latest releases.
+ - Extract the jar from the folder and drag it into `~/rootNET/addons`
+ - Please **only** use this on servers where it is allowed. I am not responsible for your actions while using this addon.
+
+# Credits
+ - All of the code here is from https://github.com/DifferentPerson/rootnet-frog-esp I just edited links and shit uwu
+ - Thanks to iZac#2346 from [rootNET](https://rootnet.dev/) for providing the Addon API.
+ - Thanks to Stronkfish#6996 for the idea (he begged for it)
+ - Thanks to Bear_03#2935 for awesome custom waifu folder integration
+ - I don't know who took the original image, or edited it, so uwu
+ - Praise be to the anime women
+ 
+ 
---
+++ src/main/java/localhost/tybie/waifuesp/WaifuEspAddon.java
@@ -0,0 +1,36 @@
+package localhost.tybie.waifuesp;
+
+import dev.rootnet.addons.api.addon.Addon;
+import dev.rootnet.addons.api.annotations.RootnetAddon;
+import org.apache.logging.log4j.Level;
+
+import java.io.IOException;
+
+/**
+ * Waifu ESP addon class
+ */
+@SuppressWarnings("unused")
+@RootnetAddon(name = "WaifuESP", author = "Tybie", version = "#VERSION#")
+public final class WaifuEspAddon extends Addon {
+
+    static final String WAIFU_URL = "https://imgur.com/7lSYRHE";
+
+    /**
+     * Will be called by the addon loader
+     */
+    public WaifuEspAddon() {
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        log(Level.INFO, "Initializing WaifuESP Addon...");
+        try {
+            getRootnet().registerModule(this, new WaifuEspModule());
+        } catch (IOException ex) {
+            log(Level.ERROR, "Failed to initialize WaifuESP");
+            ex.printStackTrace();
+        }
+    }
+
+}
